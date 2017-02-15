53537403d8e219b079ac36f44ba43031b12d079c
danielsuszek <danielsuszek@gmail.com>
2017-02-15 13:06:30 +0100

    Remove MCMDemoBundle

--- src/MCM/DemoBundle/MCMDemoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MCM\DemoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MCMDemoBundle extends Bundle
-{
-}
--- src/MCM/DemoBundle/Resources/public/js/main.js
+++
@@ -1,3 +0,0 @@
-//alert('ok');
-// 	
-//console.log("log do konsoli");
